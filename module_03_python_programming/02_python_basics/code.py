8f927232d61268b53be6eab1fa7e5421cefa4da6
trushkoladiya <trushkoladiya.work@gmail.com>
2026-03-09 19:56:45 +0530

    added module 3 chapter 2 - python basics

---
+++ module_03_python_programming/02_python_basics/code.py
@@ -0,0 +1,17 @@
+# this is what python ignores like its a not for uss and this is tells us about our code 
+# it is a comment
+
+print("Trush Koladiya") # i have to use this every time - ""
+
+# if im printing something that holding value then this - ""  is not needed
+
+# also there is a lot of keywords we can not use it like -  if, else, for, def ...
+# i have wo write meaning full names when i writing something that stores value
+
+name = input("enter your name ") # i need to store input as something so that i use it ...
+print(name)
+
+# input() is written for getting data from user ...
+
+# there is a spacing thing but i didn't understand now so may be i understand after all
+# but there is mentioned in notes that space and tab we don't have to mix
